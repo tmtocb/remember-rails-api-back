6d0280147d7fffc4892a7249e5cec4688343e174
tmtocb <kittz.esport@gmail.com>
2021-02-27 02:26:45 +0100

    add JSON:API Sources resource, controller & routing

---
+++ app/resources/source_resource.rb
@@ -0,0 +1,2 @@
+class SourceResource < JSONAPI::Resource
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   jsonapi_resources :authors
+  jsonapi_resources :remembers
+  jsonapi_resources :thoughts
+  jsonapi_resources :sources
 end
